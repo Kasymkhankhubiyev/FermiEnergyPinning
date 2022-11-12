5ba11b9ecff7e6c8bacb7fc8865ed6fe4a6f64a0
Kaseke <kasymkhankhubievnis@gmail.com>
2022-11-12 11:54:35 +0700

    created a file for implementing matplotlib api

---
+++ DrawGraphs.py
@@ -0,0 +1,8 @@
+#здесь должна юыть реализована логика отрисовки графиков
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+import matplotlib.pyplot as plt
+
+
+def run():
+    pass
